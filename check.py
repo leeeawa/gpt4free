18fdff435867e40992bb14cbe86bf4c0a955b871
leeeawa <l.eeeeee2007@gmail.com>
2023-07-20 23:16:18 +0800

    Update check.py

--- check.py
+++ check.py
@@ -24,8 +24,8 @@ while True:
               for i in p.model:
                   status['data'][-1]['model'].append({i:{'status':''}})
                   try:
-                      response = g4f.ChatCompletion.create(model=i, provider=p, messages=[{"role": "user", "content": "Hi,Please say Hey."}], stream=False)
-                      if 'Hey' in response or 'hey' in response or 'Hello' in response or 'orry' in response:
+                      response = g4f.ChatCompletion.create(model=i, provider=p, messages=[{"role": "user", "content": "Say 'Hello World!'"}], stream=False)
+                      if 'Hello World' in response or 'Hello' in response or 'hello' in response or 'world' in response:
                           status['data'][-1]['model'][-1][i]['status'] = 'Active'
                       else:
                           status['data'][-1]['model'][-1][i]['status'] = 'Inactive'
