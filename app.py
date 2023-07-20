fd5e8658905aee4a0867e3f6c71fbe7b99777001
leeeawa <l.eeeeee2007@gmail.com>
2023-07-20 21:10:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def chat_completions():
     provider = request.json.get('provider', False)
     if not provider:
         r = requests.get('https://gpt.lemonsoftware.eu.org/v1/status')
+        print(r.text)
         for p in random.shuffle(r.json()['data']):
             for m in p['model']:
                 if model in m and m[model]['status'] == 'Active':
